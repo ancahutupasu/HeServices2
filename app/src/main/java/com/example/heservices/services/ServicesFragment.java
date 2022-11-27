8876f102af953c7ed11bf4e0ca344b9931e3abef
2945245 <87025282+2945245@users.noreply.github.com>
2022-11-27 13:31:15 +0100

    updates

--- app/src/main/java/com/example/heservices/services/ServicesFragment.java
+++ app/src/main/java/com/example/heservices/services/ServicesFragment.java
@@ -2,25 +2,46 @@ package com.example.heservices.services;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.heservices.R;
+import com.example.heservices.contact.ContactViewModel;
 import com.example.heservices.express.ExpressFragment;
 
 public class ServicesFragment extends Fragment {
-
+    Button viewmore1;
+    private ServicesViewModel mViewModel;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_services, container, false);
+        View view = inflater.inflate(R.layout.fragment_services, container, false);
+
+        viewmore1 = view.findViewById(R.id.viewmore1);
+        return view;
 
 
     }
+    @Override
+    public void onActivityCreated(@Nullable Bundle saveInstanceState) {
+        super.onActivityCreated(saveInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ServicesViewModel.class);
 
+        viewmore1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //Intent intent=new Intent(this,ExpressFragment.class);
+               // startActivity(intent);
+            }
+        });
 
+    }
 }
