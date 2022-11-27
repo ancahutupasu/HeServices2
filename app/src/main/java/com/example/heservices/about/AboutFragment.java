3048fde2254a80193f13b8c6ebfd8697406e43f7
2945245 <87025282+2945245@users.noreply.github.com>
2022-11-27 12:11:28 +0100

    made account page

--- app/src/main/java/com/example/heservices/about/AboutFragment.java
+++ app/src/main/java/com/example/heservices/about/AboutFragment.java
@@ -13,5 +13,7 @@ import com.example.heservices.R;
 public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+
+
     }
 }
